c96cff20ffcbf2e966cd4723d98dd3b691e0b941
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-24 17:13:16 +0300

    added image functionality"

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
+import img from "../images/pretty-0.jpg"
+import "./about.module.css"
 
 export default () => (
   <Layout>
@@ -9,5 +11,17 @@ export default () => (
       Magni autem ex velit soluta inventore error totam molestiae minus sapiente
       cumque, laudantium minima a perferendis iste nulla non.
     </p>
+    <div className="profile">
+      <img src={img} alt="pretty babe" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        corporis debitis perferendis adipisci fuga illo hic, architecto numquam
+        molestiae ab consectetur ex molestias sequi vel totam vero, ipsa laborum
+        aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perspiciatis maiores architecto culpa eaque corrupti. Nostrum, aliquam
+        quisquam architecto temporibus id facilis labore nihil, fugit iste
+        explicabo eligendi ullam maxime corrupti.
+      </p>
+    </div>
   </Layout>
 )
