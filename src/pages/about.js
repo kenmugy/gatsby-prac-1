2662cc9cf603005c972917e91b3207f690139c44
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-24 10:02:56 +0300

    added page contents

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default () => (
+  <Layout>
+    <h1>About - Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, ad vero?
+      Magni autem ex velit soluta inventore error totam molestiae minus sapiente
+      cumque, laudantium minima a perferendis iste nulla non.
+    </p>
+  </Layout>
+)
